61919bf34e87ef425e841b5e68e2e40410ae625a
Your Name <you@example.com>
2018-04-09 03:42:27 -0500

    final task

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -10,67 +10,58 @@ import { Task } from "../models/task";
 
 export class HomeComponent  {
 
-    getAllTasks() {
+
+  constructor(@Inject(DataService) private dataService:DataService) { }
+
+  taskCount: number = 0;
+  task_title: string = '';
+  task_description: string = '';
+  task_due: string;
+  update_task = "update tasks";
+  tasks = [];
+  single_task:Task;
+  temp_task:Task;
+
+  ngOnInit() {
+    this.getAllTasks();
+    this.updateTaskText();
+  }
+
+  getAllTasks() {
       var tasksArr = this.dataService.GetAll();
       tasksArr.subscribe((value) => {
         this.tasks = value;
         this.taskCount = this.tasks.length;
-    }, error => console.log('Error Loading tasks'));
+        this.tasks.forEach(element => {
+            element.showDescription = false;
+            element.isChanged = false;
+            element.edit = false;
+        });
+      }, error => alert('Error Loading tasks'));
   }
 
-  getTaskbyId(id:number) {
-      return this.dataService.Get(1).subscribe(data => {
-        console.log(data);
-        return data;
-      }, error => console.log('Error Loading task'));
-  }
-  
-  taskCount: number = 0;
-    task_title: string = '';
-    task_description: string = '';
-    task_due: string;
-    update_task = "update tasks";
-    tasks = [];
-    single_task:Task;
-    
-    constructor(@Inject(DataService) private dataService:DataService) {
-        // this.tasks = this.dataService.GetAll().subscribe((value) => { return value});
+  getTaskbyId(id:number) { return this.dataService.Get(1).subscribe(data => { return data;}, error => alert('Error Loading task'));} 
 
-     }
-
-    ngOnInit() {
-      this.getAllTasks();
-      this.updateTaskText();
-      this.getTaskbyId(1);
-    }
-    
-
-    addTask():void {
+  addTask():void {
       if(this.task_title.length > 0) {
         this.single_task = { id : 0, title: this.task_title, description: this.task_description, done: false, due: this.convert_date(this.task_due)};
         this.dataService.Create(this.single_task).subscribe(data => {
-          console.log(data);
-        });
+          this.single_task.id = data.id;
+          alert("Successfully Created  Task");
+        }, error => alert('Error Creatting tasks'));
         this.tasks.push(this.single_task);
         this.taskCount = this.tasks.length;
         this.task_description = "";
         this.task_title = "";
         this.task_due = undefined;
         this.updateTaskText();
-        this.dataService.Delete(6).subscribe(data => {
-          console.log(data);
-        });
       }  
-    }
+  }
 
     dropTask(i):void {
       this.tasks.splice(i,1);
       this.taskCount = this.tasks.length;
       this.updateTaskText();
-    }
-
-    markDone(i):void {
-
     }
 
     convert_date(due:string) {
@@ -83,15 +74,18 @@ export class HomeComponent  {
       var temp_task =  [];
       var tasksToBeUpdated = [];
       this.tasks.forEach(element => {
-        if(element.done) {
+        if(element.isChanged) {
           tasksToBeUpdated.push(element);
         }
         else {
           temp_task.push(element);
         } 
       });
-      this.tasks = temp_task;
-      this.taskCount = this.tasks.length;
+      tasksToBeUpdated.forEach(element => {
+        this.dataService.Update(element.id, { id : element.id, title: element.title, description: element.description, done: element.done, due: this.convert_date(element.dueValue)}).subscribe(data => {          
+      }, error => alert('Error updating tasks'));
+      });
+      alert("Updated Successfully");
       this.updateTaskText();
     }
 
@@ -110,4 +104,58 @@ export class HomeComponent  {
       else false;
     }
 
+    showDescription(task) {
+      console.log(task);
+      if(task.showDescription == undefined) task.showDescription = true;
+      if(task.showDescription) task.showDescription  = false;
+      else task.showDescription = true;
+    }
+
+    changeStatus(task) {
+        if(task.isChanged == undefined) task.isChanged = false;
+        if(task.isChanged) task.isChanged  = false;
+        else task.isChanged = true;
+    }
+
+    deleteTask(task, index) {
+        this.dataService.Delete(task.id).subscribe(data => {
+          alert("Deleted Successfully");
+          this.dropTask(index);
+      }, error => alert('Error Deleting tasks'));
+    }
+
+    editTask(task) {
+      task.edit = true;
+      task.dueValue = this.reConvertDate(task.due);
+      this.temp_task = { id : task.id, title: task.title, description: task.description, done: task.done, due: this.convert_date(task.dueValue)};
+    }
+    
+    updateEditTask(task) {
+        var single_task = { id : task.id, title: task.title, description: task.description, done: task.done, due: this.convert_date(task.dueValue)};
+        this.dataService.Update(task.id, single_task).subscribe(data => {
+          alert("Updated Successfully");
+          task.edit = false;
+          task.due = single_task.due;
+      }, error => alert('Error Updating tasks'));
+    }
+
+    reConvertDate(due) {  
+      if(Date.parse(due) == NaN || due === undefined || due.length == 0) return "";
+      console.log(due);
+      var split_date = due.split("/");
+      if(split_date[2].length < 2) split_date[2] = "0" + split_date[2];
+      if(split_date[1].length < 2) split_date[1] = "0" + split_date[1];
+      if(split_date[0].length < 2) split_date[0] = "0" + split_date[0]; 
+      return split_date[2] + "-" + split_date[0] + "-" + split_date[1];
+    }
+
+    cancelEditTask(task) {
+      task.edit = false;
+      this.task_description = "";
+      this.task_title = "";
+      this.task_due = undefined;
+      task.title = this.temp_task.title;
+      task.description = this.temp_task.description; 
+      task.due = this.temp_task.due;
+    } 
 }
